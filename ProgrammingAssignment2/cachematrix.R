72937ae25ed85babe37a94c5a29f84fc60552d05
Dann <Dannhek@gmail.com>
2014-04-26 12:15:29 -0500

    cachematrix.R

---
+++ ProgrammingAssignment2/cachematrix.R
@@ -0,0 +1,36 @@
+## Filename:     cachematrix.R
+## Author:       Dann Hekman
+## Email:        dannhek@gmail.com
+## Start Date:   4/26/14
+## Initial Commit Date: n/a
+## Last Revision:       n/a
+## Purpose:      Assignment for Coursera
+##				 Johns Hopkins University RPROG-002
+##---------------------------
+## Revision History:
+## 		04/2014 - Created
+##---------------------------
+## Function:     makeCacheMatrix
+## Start Date:   4/26/14
+## Initial Commit Date: n/a
+## Last Revision:       n/a
+## Description:  description1
+##				 description2
+## Parameters:
+## 			x (I,REQ) - a matrix to cache and invert
+makeCacheMatrix <- function(x = matrix()) {
+
+}
+##---------------------------
+## Function:     cacheSolve
+## Start Date:   4/26/14
+## Initial Commit Date: n/a
+## Last Revision:       n/a
+## Description:  description1
+##				 description2
+## Parameters:
+## 			x   (I,REQ) - a matrix to cache and invert
+##          ... (I,OPT) - not currently used
+cacheSolve <- function(x, ...) {
+        ## Return a matrix that is the inverse of 'x'
+}
